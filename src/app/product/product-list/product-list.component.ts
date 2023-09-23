4628ba3cc3092b34690067f828e8f8943e54d297
Yaneth Juarez <20001023@alumnos.utleon.edu.mx>
2023-09-22 18:42:17 -0600

    22.Septiembre.23 YFJM

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProductos } from '../iproductos';
 
 @Component({
   selector: 'app-product-list',
@@ -7,7 +8,17 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
   ///Directivas *ngif If que genera una validacion dependiendo si hay una validacion o nada
-productos:any[]=[
+
+  imageWidth:number=50;
+  imageMargin:number=2;
+  muestraImg:boolean=true;
+  listFilter:string='';
+
+  showImage():void{
+    this.muestraImg=!this.muestraImg;
+  }
+
+productos:IProductos[]=[
   {
     "productoId":1,
     "Modelo":"Sentra",
@@ -16,7 +27,7 @@ productos:any[]=[
     "Precio":120000,
     "Marca":"NISSAN",
     "Color":"Morado",
-    "imagenUrl":"datos pendientes"
+    "imagenUrl":"https://www.nissanframework.com/resourcefiles/g-vehicle-detail-gallery-images/gallery-photo-4-nissan-sentra.jpg"
   },
   {
     "productoId":2,
@@ -26,7 +37,7 @@ productos:any[]=[
     "Precio":200000,
     "Marca":"AUDI",
     "Color":"Blanco",
-    "imagenUrl":"datos pendientes"
+    "imagenUrl":"https://img.remediosdigitales.com/f55210/audi-a4-2021-opiniones-prueba-mexico_/1366_2000.jpg"
   },
   {
     "productoId":3,
@@ -36,7 +47,7 @@ productos:any[]=[
     "Precio":150000,
     "Marca":"KIA",
     "Color":"Azul",
-    "imagenUrl":"datos pendientes"
+    "imagenUrl":"https://www.elcarrocolombiano.com/wp-content/uploads/2020/05/20202605-KIA-RIO-2021-PORTADA.jpg"
   },
 ]
 
