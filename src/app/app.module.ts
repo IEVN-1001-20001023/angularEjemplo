eccb4fc532a97055dec5b5b108f016d48bebce19
Yaneth Juarez <20001023@alumnos.utleon.edu.mx>
2023-10-02 13:23:20 -0600

    29.Septiembre.23 YFJM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { FormsModule } from '@angular/forms';
+import { MatInputModule} from '@angular/material/input';
+import { MatButtonModule} from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product/product-list/product-list.component';
 import { ProductoFilterPipe } from './product/producto-filter.pipe';
 import { OperasBasComponent } from './formularios/operas-bas/operas-bas.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalculosComponent } from './formularios/calculos/calculos.component';
+import { SumaComponent } from './formularios/suma/suma.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ProductoFilterPipe,
-    OperasBasComponent
+    OperasBasComponent,
+    CalculosComponent,
+    SumaComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
